e5b9f344721f4a3a82926e19ceafeccd857d4923
vertjames <ianandrewkirby@gmail.com>
2020-06-12 15:35:27 -0700

    routes started

--- models/Workout.js
+++ models/Workout.js
@@ -35,3 +35,4 @@ module.exports = model("Workout", new Schema({
     }
   ]
 })
+)
---
+++ routes/viewRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router()
+const { join } = require("path")
+
+router.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "../public/index.html"))
+})
+router.get("/stats", (req, res) => {
+  res.sendFile(join(__dirname, "../public/stats.html"))
+})
+router.get("/exercise", (req, res) => {
+  res.sendFile(join(__dirname, "../public/exercise.html"))
+})
+
+module.exports = router
